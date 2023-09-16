989a99213b54a2aacf2be5aabe5aaa5977dfe636
LimJiNa <jn4624@gmail.com>
2023-09-16 23:02:52 +0900

    initial commit

---
+++ src/main/java/com/app/paradase/ParadaseTicketWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.paradase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadaseTicketWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadaseTicketWebApplication.class, args);
+    }
+
+}
